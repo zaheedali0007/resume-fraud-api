05297d1a933c156aabafee7e6d2371fdd25b44c5
Zaheed Norman Ali <zaheedali0007@gmail.com>
2025-05-29 17:29:46 +0100

    Bind Flask app to PORT for Render deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask_cors import CORS
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -31,4 +31,5 @@ def predict():
     return jsonify({'prediction': label})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local dev
+    app.run(host='0.0.0.0', port=port)
